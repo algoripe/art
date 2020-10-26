b309fab6e850ce6483d2202b345d7864b84e5df2
algoripe <2087050f@gmail.com>
2020-10-26 22:40:28 +0100

    added more stuff

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const http = require('http');
 const express = require('express');
-const server = express();
+const app = express();
 
 const PORT = 3000;
 
-http.createServer(server);
-server.listen(PORT);
+http.createServer(app);
+app.listen(PORT);
+
+console.log(`Server running on PORT ${PORT}...\n> http://localhost:${PORT}\nPress "CTRL" + C to stop the server`);
+
+app.get('/', (req, res) => {
+    console.log(req.header);
+    res.send('Algoripe');
+});
