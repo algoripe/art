357105ffd44a445abfdc85045be56772d9743260
algoripe <2087050f@gmail.com>
2020-10-26 22:17:22 +0100

    added npm and express

---
+++ server.js
@@ -0,0 +1,8 @@
+const http = require('http');
+const express = require('express');
+const server = express();
+
+const PORT = 3000;
+
+const server = http.createServer(server);
+server.listen(PORT);
