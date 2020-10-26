a358a0116048d076928d294b2bb4356a710634fc
algoripe <2087050f@gmail.com>
2020-10-26 23:01:54 +0100

    hehe

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const http = require('http');
 const express = require('express');
 const app = express();
+const path = require('path');
 
-const PORT = 3000;
+const port = process.env.PORT || 3000;
 
 http.createServer(app);
-app.listen(PORT);
+app.listen(port);
 
-console.log(`Server running on PORT ${PORT}...\n> http://localhost:${PORT}\nPress "CTRL" + C to stop the server`);
+console.log(`Server running on port ${port}...\n> http://localhost:${port}\nPress "CTRL" + "C" to stop the server`);
 
 app.get('/', (req, res) => {
     console.log(req.header);
-    res.send('Algoripe');
+    res.sendFile(path.join(__dirname, './public/views', 'app.html'));
 });
+
