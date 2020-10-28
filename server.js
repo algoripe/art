b40589e56849b3c200283d17fc3f80b4cc913f31
algoripe <2087050f@gmail.com>
2020-10-28 21:59:20 +0100

    worked on server

--- server.js
+++ server.js
@@ -1,17 +1,39 @@
 const http = require('http');
 const express = require('express');
-const app = express();
 const path = require('path');
 
+
+const server = express();
 const port = process.env.PORT || 3000;
-
-http.createServer(app);
-app.listen(port);
-
+http.createServer(server);
+server.listen(port);
 console.log(`Server running on port ${port}...\n> http://localhost:${port}\nPress "CTRL" + "C" to stop the server`);
 
-app.use(express.static("public"));
+// SHOULD BE ANOTHER FILE
+const router = express.Router();
+const sketchRoutes = require('./routes/sketch');
 
-app.get('/', (req, res) => {
+
+server.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if (req.method === "OPTIONS") {
+      res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+      return res.status(200).json({});
+    }
+    next();
+});
+
+server.use(express.static("public"));
+
+server.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/views', 'app.html'));
 });
+
+// ONLY THE JS FILE OF SKETCH PAGE SHOULD CHANGE
+// server.get('/drawing/:drawingId', (req, res) => {
+//     res.sendFile(path.join(__dirname, './public/views', 'app.html'));
+// });
