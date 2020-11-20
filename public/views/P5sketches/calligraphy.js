9cd00da6547b2ac91507424d63b6930e264f324e
algoripe <2087050f@gmail.com>
2020-11-20 17:16:54 +0100

    improved calligraphy sketch, added onebyoneletter animation function

--- public/views/P5sketches/calligraphy.js
+++ public/views/P5sketches/calligraphy.js
@@ -6,7 +6,7 @@ let height = width;
 let canvas;
 
 // // BACKGROUND AND STROKE
-const backgroundClr = `#000000`;
+const backgroundClr = `#ffffff`;
 
 // // FRAMERATE
 // const frameRateValue = 30;
@@ -16,16 +16,60 @@ let t = 0;
 let SLOW_INCREMENT = 0;
 
 // particle system
-const NUMBER_OF_AUTOMATAS = 50;
+const NUMBER_OF_AUTOMATAS = 10;
 // const PARTICLE_JOIN_DISTANCE = noise(t + 300) * 50 + 40;
 // const PARTICLE_JOIN_DISTANCE = 20 + width / NUMBER_OF_AUTOMATAS;
-const DEFAULT_PARTICLE_JOIN_DISTANCE = 25;
+let DEFAULT_PARTICLE_JOIN_DISTANCE = 40;
+
+// TYPOGRAPHY
+let displayText;
+const frameCountPerLetter = 25;
 
 const letterLines = {
   A: [
     [0.5, 0.2, 0.2, 0.8],
     [0.5, 0.2, 0.8, 0.8],
     [0.3, 0.6, 0.7, 0.6],
+  ],
+  B: [
+    [0.2, 0.2, 0.2, 0.8],
+    [0.2, 0.2, 0.5, 0.2],
+    [0.5, 0.2, 0.7, 0.3],
+    [0.7, 0.3, 0.7, 0.4],
+    [0.7, 0.4, 0.6, 0.5],
+    [0.6, 0.5, 0.3, 0.5],
+    [0.2, 0.8, 0.7, 0.8],
+    [0.7, 0.8, 0.8, 0.7],
+    [0.8, 0.7, 0.8, 0.6],
+    [0.8, 0.6, 0.7, 0.5],
+  ],
+  C: [
+    [0.8, 0.4, 0.5, 0.2],
+    [0.5, 0.2, 0.2, 0.4],
+    [0.2, 0.6, 0.2, 0.4],
+    [0.2, 0.6, 0.5, 0.8],
+    [0.5, 0.8, 0.8, 0.6],
+  ],
+  E: [
+    [0.2, 0.2, 0.8, 0.2],
+    [0.3, 0.5, 0.6, 0.5],
+    [0.2, 0.8, 0.8, 0.8],
+    [0.3, 0.2, 0.3, 0.8],
+  ],
+  F: [
+    [0.2, 0.2, 0.8, 0.2],
+    [0.2, 0.5, 0.7, 0.5],
+    [0.2, 0.2, 0.2, 0.8],
+  ],
+  G: [
+    [0.8, 0.3, 0.7, 0.2],
+    [0.8, 0.2, 0.4, 0.2],
+    [0.4, 0.2, 0.2, 0.4],
+    [0.2, 0.4, 0.2, 0.7],
+    [0.2, 0.7, 0.4, 0.8],
+    [0.4, 0.8, 0.8, 0.8],
+    [0.8, 0.8, 0.8, 0.6],
+    [0.5, 0.6, 0.8, 0.6],
   ],
   H: [
     [0.2, 0.2, 0.2, 0.8],
@@ -36,6 +80,54 @@ const letterLines = {
     [0.3, 0.2, 0.7, 0.2],
     [0.5, 0.2, 0.5, 0.8],
     [0.3, 0.8, 0.7, 0.8],
+  ],
+  K: [
+    [0.2, 0.2, 0.2, 0.8],
+    [0.2, 0.5, 0.8, 0.2],
+    [0.25, 0.45, 0.8, 0.8],
+  ],
+  L: [
+    [0.2, 0.2, 0.2, 0.8],
+    [0.8, 0.8, 0.2, 0.8],
+  ],
+  N: [
+    [0.2, 0.2, 0.2, 0.8],
+    [0.2, 0.2, 0.8, 0.8],
+    [0.8, 0.2, 0.8, 0.8],
+  ],
+  O: [
+    [0.2, 0.5, 0.5, 0.2],
+    [0.5, 0.2, 0.8, 0.5],
+    [0.8, 0.5, 0.5, 0.8],
+    [0.5, 0.8, 0.2, 0.5],
+  ],
+  P: [
+    [0.2, 0.2, 0.2, 0.8],
+    [0.2, 0.2, 0.6, 0.2],
+    [0.6, 0.2, 0.8, 0.3],
+    [0.7, 0.25, 0.7, 0.45],
+    [0.8, 0.3, 0.8, 0.4],
+    [0.8, 0.4, 0.6, 0.5],
+    [0.6, 0.5, 0.3, 0.5],
+  ],
+  R: [
+    [0.2, 0.2, 0.2, 0.8],
+    [0.2, 0.2, 0.6, 0.2],
+    [0.6, 0.2, 0.8, 0.3],
+    [0.7, 0.25, 0.7, 0.45],
+    [0.8, 0.3, 0.8, 0.4],
+    [0.8, 0.4, 0.6, 0.5],
+    [0.6, 0.5, 0.3, 0.5],
+    [0.8, 0.8, 0.4, 0.5],
+  ],
+  T: [
+    [0.2, 0.2, 0.8, 0.2],
+    [0.5, 0.2, 0.5, 0.8],
+  ],
+  Y: [
+    [0.2, 0.2, 0.5, 0.5],
+    [0.8, 0.2, 0.5, 0.5],
+    [0.5, 0.8, 0.5, 0.5],
   ],
 };
 const automatas = [];
@@ -48,36 +140,38 @@ class Automata {
     yPosition,
     xDirection,
     yDirection,
-    joinDistance
+    joinDistance,
+    automataColor,
+    type
   ) {
     if (size !== null) {
       this.size = size;
     } else {
-      this.size = 1;
+      this.size = random(1, 7);
     }
 
     if (xPosition !== null) {
       this.xPosition = xPosition;
     } else {
-      this.xPosition = width * Math.random();
+      this.xPosition = 15 + (width - 20) * Math.random();
     }
 
     if (yPosition !== null) {
       this.yPosition = yPosition;
     } else {
-      this.yPosition = height * Math.random();
+      this.yPosition = 15 + (height - 20) * Math.random();
     }
 
     if (xDirection !== null) {
       this.xDirection = xDirection;
     } else {
-      this.xDirection = random(-3, 3);
+      this.xDirection = random(-1, 1);
     }
 
     if (yDirection !== null) {
       this.yDirection = yDirection;
     } else {
-      this.yDirection = random() > 0.5 ? 1 : -1;
+      this.yDirection = random(-1, -20);
     }
 
     if (joinDistance !== null) {
@@ -85,6 +179,29 @@ class Automata {
     } else {
       this.joinDistance = DEFAULT_PARTICLE_JOIN_DISTANCE;
     }
+
+    if (automataColor !== null && automataColor !== undefined) {
+      const r = automataColor[0];
+      const g = automataColor[1];
+      const b = automataColor[2];
+      this.color = color(r, g, b);
+    } else {
+      this.color = color(random(0, 100), random(5), random(0, 100));
+    }
+
+    if (type !== null && type !== undefined) {
+      this.type = type;
+    } else {
+      this.type = "DEFAULT";
+    }
+
+    if (this.type === "DEFAULT") {
+      this.strokeW = 0.2;
+    } else {
+      this.strokeW = 0.9;
+    }
+
+    this.uniqueNumber = random() * 10000;
 
     // this.yDirection = random(-3, 3);
     // this.xDirection = random() > 0.5 ? 1 : -1;
@@ -97,17 +214,14 @@ class Automata {
     // } else {
     //   this.color = c2;
     // }
-    this.color = color(random(250, 255), random(55), random(250, 255));
   }
 
   move = () => {
     if (this.xDirection === 0 && this.yDirection === 0) {
       this.xPosition =
-        this.xPosition +
-        cos(SLOW_INCREMENT * TWO_PI + this.color.levels[0]) / 30;
+        this.xPosition + cos(SLOW_INCREMENT * TWO_PI + this.uniqueNumber) / 20;
       this.yPosition =
-        this.yPosition +
-        sin(SLOW_INCREMENT * TWO_PI + this.color.levels[2]) / 2;
+        this.yPosition + sin(SLOW_INCREMENT * TWO_PI + this.uniqueNumber) / 20;
     } else {
       this.xPosition += this.xDirection;
       this.yPosition += this.yDirection;
@@ -128,12 +242,14 @@ class Automata {
   };
 
   draw = () => {
-    // stroke("#fff");
-    stroke(this.color);
-    // strokeWeight(this.size);
+    stroke("#000");
+    strokeWeight(0.7);
     // point(this.xPosition, this.yPosition);
-    fill(color(random(150, 255), random(55), random(150, 255)));
+    // stroke(this.color);
+
+    fill(color(noise(t + this.xPosition) * 255, 0, noise(t) * 255));
     circle(this.xPosition, this.yPosition, this.size);
+    noFill();
   };
 
   joinParticles = (currentAutomataIndex) => {
@@ -144,28 +260,27 @@ class Automata {
     automatas.forEach((i) => {
       let dis = dist(this.xPosition, this.yPosition, i.xPosition, i.yPosition);
       if (dis < this.joinDistance && dis > 2) {
-        noFill();
-        strokeWeight(0.6);
-        stroke(this.color);
+        strokeWeight(this.strokeW);
+        stroke(i.color);
         // stroke(random(50, 255), random(0, 105), random(200, 255));
 
         const bezX1 =
-          (noise(this.color.levels[0] + t) - 0.5) *
+          (noise(this.uniqueNumber + t) - 0.5) *
             (DEFAULT_PARTICLE_JOIN_DISTANCE / 2) +
           (this.xPosition + i.xPosition + 3 * randomGaussian()) / 2;
 
         const bezY1 =
-          (noise(this.color.levels[1] + t) - 0.5) *
+          (noise(this.uniqueNumber + t) - 0.5) *
             (DEFAULT_PARTICLE_JOIN_DISTANCE / 2) +
           (this.yPosition + i.yPosition + 3 * randomGaussian()) / 2;
 
         const bezX2 =
-          (noise(this.color.levels[2] + t) - 0.5) *
+          (noise(this.uniqueNumber + t) - 0.5) *
             (DEFAULT_PARTICLE_JOIN_DISTANCE / 2) +
           (this.xPosition + i.xPosition) / 2;
 
         const bezY2 =
-          (noise(this.color.levels[0] + t + 255) - 0.5) *
+          (noise(this.uniqueNumber + t + 255) - 0.5) *
             (DEFAULT_PARTICLE_JOIN_DISTANCE / 2) +
           (this.yPosition + i.yPosition) / 2;
 
@@ -180,25 +295,25 @@ class Automata {
           i.yPosition
         );
 
-        stroke(i.color);
+        stroke(this.color);
 
         const bez2X1 =
-          (noise(this.color.levels[0] + t + 1) - 0.5) *
+          (noise(this.uniqueNumber + t + 1) - 0.5) *
             (DEFAULT_PARTICLE_JOIN_DISTANCE / 4) +
           (this.xPosition + i.xPosition + 3 * randomGaussian()) / 2;
 
         const bez2Y1 =
-          (noise(this.color.levels[1] + t + 2) - 0.5) *
+          (noise(this.uniqueNumber + t + 2) - 0.5) *
             (DEFAULT_PARTICLE_JOIN_DISTANCE / 4) +
           (this.yPosition + i.yPosition + 3 * randomGaussian()) / 2;
 
         const bez2X2 =
-          (noise(this.color.levels[2] + t + 3) - 0.5) *
+          (noise(this.uniqueNumber + t + 3) - 0.5) *
             (DEFAULT_PARTICLE_JOIN_DISTANCE / 4) +
           (this.xPosition + i.xPosition) / 2;
 
         const bez2Y2 =
-          (noise(this.color.levels[0] + t + 4) - 0.5) *
+          (noise(this.uniqueNumber + t + 4) - 0.5) *
             (DEFAULT_PARTICLE_JOIN_DISTANCE / 4) +
           (this.yPosition + i.yPosition) / 2;
 
@@ -254,7 +369,7 @@ class PointLetter {
       y = arr[1] * this.LetterHeight;
       // console.log(key);
       // console.log(x, y);
-      const a = new Automata(1, x, y, 0, 0);
+      const a = new Automata(1, x, y, 0, 0, null, LETTER_POINT);
       automatas.push(a);
       // a.draw();
     }
@@ -263,8 +378,9 @@ class PointLetter {
 
 class LineLetter {
   constructor(lines, width, height, x, y) {
+    this.letterId = random(10000);
     this.lines = lines;
-    console.log(lines);
+    // console.log(lines);
     for (const i of lines) {
       const x1 = x + i[0] * width;
       const y1 = y + i[1] * height;
@@ -273,18 +389,31 @@ class LineLetter {
       // stroke("#fff");
       // strokeWeight(3);
       // console.log(x1, y1, x2, y2);
-      automatas.push(new Automata(null, x1, y1, 0, 0, 0));
-      automatas.push(new Automata(null, x2, y2, 0, 0, 0));
+      automatas.push(
+        new Automata(null, x1, y1, 0, 0, 0, [0, 0, 0], this.letterId)
+      );
+      automatas.push(
+        new Automata(null, x2, y2, 0, 0, 0, [0, 0, 0], this.letterId)
+      );
       this.addAutomataHalfway(x1, y1, x2, y2);
     }
   }
 
   addAutomataHalfway = (x1, y1, x2, y2) => {
-    if (dist(x1, y1, x2, y2) > DEFAULT_PARTICLE_JOIN_DISTANCE) {
+    if (dist(x1, y1, x2, y2) > 40) {
       const halfX = (x1 + x2) / 2;
       const halfY = (y1 + y2) / 2;
       automatas.push(
-        new Automata(null, halfX, halfY, 0, 0, DEFAULT_PARTICLE_JOIN_DISTANCE)
+        new Automata(
+          null,
+          halfX,
+          halfY,
+          0,
+          0,
+          DEFAULT_PARTICLE_JOIN_DISTANCE,
+          [random(0, 50), 0, random(0, 50)],
+          this.letterId
+        )
       );
       this.addAutomataHalfway(x1, y1, halfX, halfY);
       this.addAutomataHalfway(halfX, halfY, x2, y2);
@@ -302,51 +431,17 @@ function setup() {
   noFill();
 
   // BACKGROUND COLOR
-  // background(backgroundClr);
+  background(backgroundClr);
 
   // NOISE
   // noiseSeed(1999);
 
   for (let i = 0; i < NUMBER_OF_AUTOMATAS; i++) {
-    automatas[i] = new Automata(null, null, null, null, null, null);
+    automatas.push(new Automata(null, null, null, null, null, null));
     automatas[i].draw();
   }
 
-  // const letterA = new Letter({
-  //     a: [0.2, 0.8],
-  //     b: [0.25, 0.7],
-  //     p2: [0.45, 0.35],
-  //     p3: [0.5, 0.2],
-  //     p4: [0.8, 0.8],
-  //     p5: [0.3, 0.6],
-  //     p6: [0.5, 0.6],
-  //     // p7: [0.6, 0.6],
-  //     p8: [0.7, 0.6],
-  //     p9: [0.55, 0.3],
-  //   },
-  //   width,
-  //   height,
-  //   0,
-  //   0
-  // );
-
-  const letterH = new LineLetter(letterLines.H, width / 2, height, 0, 0);
-
-  const letterI = new LineLetter(
-    letterLines.I,
-    width / 2,
-    height,
-    width / 2,
-    0
-  );
-
-  // const letterH = new LineLetter(
-  //   letterLines.H,
-  //   width / 4,
-  //   height / 2,
-  //   width / 8,
-  //   height / 2
-  // );
+  displayText = "ABCEFGHIKLNOPRTY";
 }
 
 function draw() {
@@ -355,19 +450,137 @@ function draw() {
   for (let i = 0; i < automatas.length; i++) {
     const automata = automatas[i];
 
-    automata.draw();
+    // automata.draw();
     automata.move();
     automata.joinParticles(i);
+    // automata.draw();
   }
 
   // POINTS FOR DRAWING
+  // const letterA = new LineLetter(letterLines.A, width / 4, height / 2, 0, 0);
 
   // INCREMENTS
   t += 0.15;
   SLOW_INCREMENT += 0.005;
 
-  if (mouseX > 0 && mouseX < width && mouseY > 0 && mouseY < height) {
-    automatas[0].xPosition = mouseX;
-    automatas[0].yPosition = mouseY;
+  if (frameCount === 1) {
+    const ltrA = new LineLetter(letterLines.A, width / 4, height / 2, 0, 0);
+    const ltrL = new LineLetter(
+      letterLines.L,
+      width / 4,
+      height / 2,
+      width / 4,
+      0
+    );
+    const ltrG = new LineLetter(
+      letterLines.G,
+      width / 4,
+      height / 2,
+      width / 2,
+      0
+    );
+    const ltrO = new LineLetter(
+      letterLines.O,
+      width / 4,
+      height / 2,
+      (3 * width) / 4,
+      0
+    );
+    const ltrF = new LineLetter(
+      letterLines.F,
+      width / 4,
+      height / 2,
+      0,
+      height / 2
+    );
+    const ltrO1 = new LineLetter(
+      letterLines.O,
+      width / 4,
+      height / 2,
+      width / 4,
+      height / 2
+    );
+    const ltrN = new LineLetter(
+      letterLines.N,
+      width / 4,
+      height / 2,
+      width / 2,
+      height / 2
+    );
+    const ltrT = new LineLetter(
+      letterLines.T,
+      width / 4,
+      height / 2,
+      (3 * width) / 4,
+      height / 2
+    );
+  } else if (
+    frameCount > 100 &&
+    frameCount < frameCountPerLetter * displayText.length + 100
+  ) {
+    oneByOneLetters(displayText.split(""), frameCountPerLetter, 101);
   }
+
+  // if (mouseX > 0 && mouseX < width && mouseY > 0 && mouseY < height) {
+  //   automatas[0].xPosition = mouseX;
+  //   automatas[0].yPosition = mouseY;
+  // }
 }
+
+// FUNCTIONS
+
+const stringToLetters = (string) => {
+  const inputString = `${string}`;
+  const inputStringArray = inputString.split("");
+  // console.log(inputStringArray);
+  const outputStringArray = [];
+  for (let i = 0; i < inputStringArray.length; i++) {
+    if (letterLines.hasOwnProperty(`${inputStringArray[i]}`)) {
+      outputStringArray.push(inputStringArray[i]);
+      // console.log(outputStringArray);
+    } else {
+      console.log("couldn't get the right letterlines");
+    }
+  }
+
+  // const maxChar = 4; // maximum nb of characters per row
+  // const maxRows = 2; // maximum nb of rows
+  // let rows;
+
+  for (let j = 0; j < outputStringArray.length; j++) {
+    const lines = letterLines[outputStringArray[j]];
+    const letterWidth =
+      outputStringArray.length === 1
+        ? width / 2
+        : width / outputStringArray.length;
+    const letterHeight = height;
+    const posX = outputStringArray.length === 1 ? width / 4 : letterWidth * j;
+    const posY = 0;
+    letters.push(new LineLetter(lines, letterWidth, letterHeight, posX, posY));
+  }
+};
+
+const removeLetterPoints = () => {
+  for (let i = 0; i < automatas.length; i++) {
+    if (automatas[i].type !== "DEFAULT") {
+      automatas.splice(i);
+      letters.splice(0, letters.length);
+    }
+  }
+};
+
+const oneByOneLetters = (lettersArray, framesPerLetter, frameToStartOn) => {
+  if (frameToStartOn < 1) {
+    console.log(
+      `variable FrameToStartOn should be greater than or equal to 1. Current value: ${frameToStartOn}`
+    );
+    return;
+  }
+  for (let i = 0; i < lettersArray.length; i++) {
+    if (frameCount === frameToStartOn + framesPerLetter * i) {
+      removeLetterPoints();
+      stringToLetters(lettersArray[i]);
+      console.log(i, framesPerLetter * i + 1, lettersArray[i]);
+    }
+  }
+};
